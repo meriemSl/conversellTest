66b9c6e74c2fe45bc7b271deb88fbde3069128f7
meriemslimany <meriemslimany@confledis.com>
2021-03-15 16:29:19 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,181 @@
+    const dfMessenger = document.querySelector('df-messenger');
+    dfMessenger.addEventListener('df-button-clicked', function (event) {
+        
+        var elem,
+        style,textTest;
+        textTest = `df-messenger { 
+                                    --df-messenger-bot-message: #fff;
+                                    --df-messenger-button-titlebar-color: #0041C2;
+                                    --df-messenger-chat-background-color: #0041C2;
+                                    --df-messenger-font-color: #000;
+                                    --df-messenger-send-icon: #00adff;
+                                    --df-messenger-user-message: #0041C2;
+                                    --df-messenger-chip-border-color:#0041C2;
+                                } `;
+        
+        elem = document.querySelector('df-messenger');
+        console.log(event)
+    
+    
+    
+        
+            const payload = [
+                {
+                  "type": "info",
+                  "title": "Info item title",
+                  "subtitle": "Info item subtitle",
+                  "image": {
+                    "src": {
+                      "rawUrl": "https://example.com/images/logo.png"
+                    }
+                  },
+                  "actionLink": "https://example.com"
+                }];
+              dfMessenger.renderCustomCard(payload);
+        
+        
+    //console.log(document.styleSheets)
+    //    //console.log(elem)
+    //    //console.log(textTest);
+    //    // render it to the same value 
+    //    elem.textContent = textTest ;
+    //    console.log(elem.textContent);
+    //    // read only you can't change it value 
+    //    style = getComputedStyle(elem);
+    //    console.log((style));
+    //    console.log((style).getPropertyValue("--df-messenger-chat-background-color"));
+       
+    //    console.log(document.getElementsByClassName('df-messenger'));
+    //   // access to the attribute which is working but with static value 
+    //   console.log( dfMessenger.getAttribute("expand")  )
+    //   dfMessenger.getAttribute("agent-id") ;
+    //   dfMessenger.setAttribute("agent-id","d335be4c-9388-4b51-9616-6a9c130962da")
+    //   console.log(dfMessenger.getAttribute("agent-id"));
+    //   expandV = true
+    //   dfMessenger.setAttribute("expand", expandV ); 
+    //   console.log( dfMessenger.getAttribute("expand")  ); 
+       
+       
+    //   dfMessenger.getAttribute("chat-icon") ;
+    //   dfMessenger.setAttribute("chat-icon","https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F85%2FCircle-icons-chat.svg%2F1024px-Circle-icons-chat.svg.png&f=1&nofb=1")
+    //   console.log(dfMessenger.getAttribute("chat-icon"));
+       
+       
+    //   dfMessenger.textContent = "Go to GeeksforGeeks"; 
+        
+          
+    
+    });
+    
+    dfMessenger.addEventListener('df-chip-clicked', function (event) {
+        console.log(event.detail.query)
+        //var result = (event.detail.query).localeCompare("text");
+       // console.log(result);
+     switch(event.detail.query) {
+      
+        case 'show me text':    {
+            let request = new XMLHttpRequest()
+            //http://localhost:3000/textEventConsume
+            request.open("GET","https://us-central1-conversell-pfe-21.cloudfunctions.net/app/textEventConsume");
+            request.send();
+            request.onload = () => {
+            if(request.status === 200){
+            console.log(request.response)
+            dfMessenger.renderCustomText((request.response).toString())
+            } else {
+            console.log("Page not found")// if link is broken, output will be page not found
+                  }
+            }    
+        }
+        break; 
+        
+        case 'show me card':{
+            
+            let request = new XMLHttpRequest()
+            request.open("GET","https://us-central1-conversell-pfe-21.cloudfunctions.net/app/payloadeventConsume");
+            request.send();
+            request.onload = () => {
+            if(request.status === 200){
+            console.log(JSON.parse(request.response))
+            dfMessenger.renderCustomCard(JSON.parse(request.response))
+            } else {
+            console.log("Page not found")// if link is broken, output will be page not found
+            }
+            }
+        }
+        break;
+    }
+        
+      });
+    
+    dfMessenger.addEventListener('df-list-element-clicked', function (event) {
+        console.log(event.detail.element.event.name );
+        
+       if (event.detail.element.event.name == "item1")
+          {
+            const payload = [
+                {
+                  "type": "info",
+                  "title": "info item from list ",
+                  "subtitle": "list item subtitle",
+                  "image": {
+                    "src": {
+                      "rawUrl": "https://example.com/images/logo.png"
+                    }
+                  },
+                  "actionLink": "https://example.com"
+                }];
+              dfMessenger.renderCustomCard(payload);
+          }
+          else {
+            const payload = [
+                
+                        {
+                          "type": "description",
+                          "title": "Description title",
+                          "text": [
+                            "This is text line 1.",
+                            "This is text line 2."
+                          ]
+                        }
+                      
+                    
+                  
+                  
+              ];
+              dfMessenger.renderCustomCard(payload);
+    
+          }
+    
+    
+    //    let request = new XMLHttpRequest()
+    //    request.open("GET","http://localhost:3000/detectIntent");
+    //    request.send();
+    //    request.onload = () => {
+    //    if(request.status === 200){
+    //         console.log(JSON.parse(request.response))
+    //    //dfMessenger.renderCustomCard(JSON.parse(request.response))
+    //    } else {
+    //         console.log("Page not found")// if link is broken, output will be page not found
+    //    } 
+    //}   
+    
+    colorPicker.addEventListener("input", updateFirst, false);
+    colorPicker.addEventListener("change", watchColorPicker, false);
+    
+    function watchColorPicker(event) {
+      document.querySelectorAll("p").forEach(function(p) {
+        p.style.color = event.target.value;
+      });
+    }
+    });
+    
+    
+      
+    
+    
+     
+    
+    
+    
+    
